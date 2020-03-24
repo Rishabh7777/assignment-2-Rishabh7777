c9b1338f36137c553f00fe5708f608e020599698
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-24 11:45:48 +0530

    check method insert()

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,26 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+
+        System.out.println(tree.getRootNode());
+        tree.insert(70);
+        tree.insert(35);
+        tree.insert(93);
+        tree.insert(19);
+        tree.insert(60);
+        tree.insert(79);
+        tree.insert(108);
+        tree.insert(11);
+        tree.insert(28);
+        tree.insert(64);
+        tree.insert(99);
+        tree.insert(111);
+        System.out.println(tree.getRootNode());
+    }
 }
