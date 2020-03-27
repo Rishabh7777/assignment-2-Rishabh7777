f966a855714fb7c11ca23ad94fb88f187001fb45
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-27 22:15:53 +0530

    add constructor and getter-setters

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,30 @@
 package problem3.node;
 
 public class Node {
+
+    private int data;   // data that each node carries
+    private Node next;  // pointer to next node
+
+    // constructor
+    public Node(int data) {
+        this.data = data;
+        next = null;    // initially next pointing to null
+    }
+
+    // getters and setters
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
