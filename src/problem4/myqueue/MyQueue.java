8ac9f241ed2036f3bedaa45fabc9e9edb4f90925
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-27 22:57:11 +0530

    add fields and constructor

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,18 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.node.Node;
+
 public class MyQueue {
+
+    private Node front;
+    private Node rear;  // point to the last element of queue
+    private int size;
+
+    // constructor
+    MyQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
 }
