e84f2003c85b498a2b025b9b6f525b7d1fa5bb8c
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-27 09:12:43 +0530

    add method preOrderTraversal()

--- src/problem2/mybst/MyBinarySearchTree.java
+++ src/problem2/mybst/MyBinarySearchTree.java
@@ -2,35 +2,17 @@ package problem2.mybst;
 
 import problem1.node.TreeNode;
 
-public class MyBinarySearchTree {
+public class MyBinarySearchTree extends problem1.mybst.MyBinarySearchTree {
 
-    private TreeNode rootNode;
+    // it prints root node then left child and then right child
+    public void preOrderTraversal(TreeNode node) {
 
-    public TreeNode getRootNode() {
-        return rootNode;
-    }
-
-    public void insert(int data) {
-        TreeNode node = new TreeNode(data);
-        if (rootNode == null) {
-            rootNode = node;
-        } else {
-            TreeNode temp = rootNode;
-            TreeNode parentNode = null;
-            while (temp != null) {
-                parentNode = temp;
-                if (node.getData() < temp.getData()) {
-                    temp = temp.getLeft();
-                } else {
-                    temp = temp.getRight();
-                }
-            }
-            // setting child node
-            if (node.getData() < parentNode.getData()) {
-                parentNode.setLeft(node);
-            } else {
-                parentNode.setRight(node);
-            }
+        // checking if given node is non-empty
+        if (node != null) {
+            System.out.print(node.getData() + ", ");
+            preOrderTraversal(node.getLeft());  // calling method again for left child of node
+            preOrderTraversal(node.getRight()); // calling method again for right child of node
         }
+
     }
 }
