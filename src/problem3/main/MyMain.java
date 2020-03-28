a63f3fab61121205df500982731094e655f0be99
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-28 15:08:02 +0530

    check method insertData()

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.linkedlist.LinkedList;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.insertData(new Student("Rahul", 34));
+        linkedList.insertData(new Student("Amit", 2));
+    }
 }
