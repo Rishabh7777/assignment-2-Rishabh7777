04f957cbfb55cec50a5f09627fcf4dbd0a708e3c
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-28 14:30:18 +0530

    add fields name and rollNumber and constructor

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -1,4 +1,13 @@
 package problem3.student;
 
 public class Student {
+
+    private String name;
+    private int rollNumber;
+
+    // constructor
+    public Student(String name, int rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
 }
