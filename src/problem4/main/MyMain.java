ef82bc0b411f8b3068cb3445013ef5776ec8d5d9
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-28 11:25:39 +0530

    check method printResultantQueue()

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,40 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem4.bst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+
+        bst.insert(70);
+        bst.insert(35);
+        bst.insert(93);
+        bst.insert(19);
+        bst.insert(60);
+        bst.insert(79);
+        bst.insert(108);
+        bst.insert(11);
+        bst.insert(28);
+        bst.insert(64);
+        bst.insert(99);
+        bst.insert(111);
+        bst.insert(5);
+        bst.insert(17);
+        bst.insert(31);
+        bst.insert(3);
+        bst.insert(63);
+        bst.insert(82);
+        bst.insert(106);
+        bst.insert(109);
+        bst.insert(110);
+
+        bst.printResultantQueue(bst.getRootNode());
+    }
+
+
 }
