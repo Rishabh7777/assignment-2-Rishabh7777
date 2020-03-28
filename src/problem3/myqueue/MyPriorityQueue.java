046c6f25fd2b8882f13156cbbee3e38a8cd28aec
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-28 16:00:23 +0530

    add insert, delete and print methods

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,22 @@
  */
 package problem3.myqueue;
 
+import problem3.linkedlist.LinkedList;
+import problem3.student.Student;
+
 public class MyPriorityQueue {
+
+    LinkedList linkedList = new LinkedList();
+
+    public void insertInQueue(Student student) {
+        linkedList.insertAtLast(student);
+    }
+
+    public String deleteFromQueue() {
+        return linkedList.deleteFromBeginning();
+    }
+
+    public void printQueue() {
+        linkedList.printData();
+    }
 }
