69157a593ed546c407cc13efc3f6701b0640d99c
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-29 16:06:45 +0530

    check method insertInQueue()

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,19 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyCircularQueue circularQueue = new MyCircularQueue(5);
+
+        circularQueue.insertInQueue(new Student("Ajay", 2, 0));
+        circularQueue.insertInQueue(new Student("Rahul", 1, 1));
+        circularQueue.insertInQueue(new Student("Sonu", 0, 0));
+        circularQueue.insertInQueue(new Student("Amit", 2, 2));
+    }
 }
