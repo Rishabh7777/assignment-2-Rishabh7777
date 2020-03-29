715e1a520b1efd5e63bf88ad16d4e24449ed6f5c
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-29 15:22:28 +0530

    add fields and constructor

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,18 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+
+    private Student data;
+    private Node next;
+
+    // constructor
+    public Node(Student data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 }
