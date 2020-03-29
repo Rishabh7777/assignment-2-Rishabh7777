54a56220e7cf2782154042b52883f073f347b457
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-29 16:13:58 +0530

    check method deleteFromQueue()

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -9,6 +9,8 @@ package problem5.main;
 import problem5.circularqueue.MyCircularQueue;
 import problem5.student.Student;
 
+import java.util.Arrays;
+
 //executable class
 public class MyMain {
     public static void main(String[] args) {
@@ -19,5 +21,8 @@ public class MyMain {
         circularQueue.insertInQueue(new Student("Rahul", 1, 1));
         circularQueue.insertInQueue(new Student("Sonu", 0, 0));
         circularQueue.insertInQueue(new Student("Amit", 2, 2));
+        System.out.println(Arrays.toString(circularQueue.printQueue()));
+
+        circularQueue.deleteFromQueue();
     }
 }
