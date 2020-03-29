cfd6498d73515f64c64b76d5bf4829ddafaa6c4a
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-29 15:45:41 +0530

    updated fields and respective methods

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,14 +7,25 @@
 package problem5.student;
 // to store student information and properties
 
-public class Student extends problem3.student.Student {
+public class Student {
 
+    private String name;
     private int backLog;
+    private int appearingCount;
 
     // constructor
-    public Student(String name, int rollNUmber, int backLog) {
-        super(name, rollNUmber);
+    public Student(String name, int backLog, int appearingCount) {
+        this.name = name;
         this.backLog = backLog;
+        this.appearingCount = appearingCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getBackLog() {
@@ -25,7 +36,15 @@ public class Student extends problem3.student.Student {
         this.backLog = backLog;
     }
 
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
+
     public String toString() {
-        return getName() + ", " + getRollNumber() + ": " + getBackLog() + "(backlogs)";
+        return getName() + ": " + getBackLog();
     }
 }
