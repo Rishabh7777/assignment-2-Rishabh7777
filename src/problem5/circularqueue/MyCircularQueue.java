307d841db179c01b6676c53d1aa0d04597254293
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-03-29 16:02:42 +0530

    add method deleteFromQueue()

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,25 +6,27 @@
  */
 package problem5.circularqueue;
 
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
 
-    private int[] circularQueue;
+    private Student[] circularQueue;
     private int front;
     private int rear;
     private int size;
 
     public MyCircularQueue(int sizeOfArray) {
-        circularQueue = new int[sizeOfArray];
+        circularQueue = new Student[sizeOfArray];
         front = 0;
         rear = 0;
         size = 0;
     }
 
-    public void insertInQueue(int element) {
+    public void insertInQueue(Student data) {
         if (rear < circularQueue.length) { // queue.length will always equal to initialised size of queue
 //            System.out.println(queue.length);
-            circularQueue[rear] = element;
+            circularQueue[rear] = data;
             rear++;
             size++;
             if (rear == circularQueue.length && size < circularQueue.length) {
@@ -33,5 +35,30 @@ public class MyCircularQueue {
         } else {
             System.out.println("Queue is full.");
         }
+    }
+
+    public void deleteFromQueue() {
+        if (!isEmpty() && circularQueue[front].getBackLog() == circularQueue[front].getAppearingCount()) {
+            front++;
+            size--;
+            if (front == circularQueue.length && size < circularQueue.length) {
+                front = 0;
+            }
+            if (rear == circularQueue.length && size < circularQueue.length) {
+                rear = 0;
+            }
+        } else if (isEmpty()) {
+            System.out.println("Queue is empty.");
+        } else {
+            System.out.println(circularQueue[front].getName() + " has not appeared in all backlogs.");
+        }
+    }
+
+    private boolean isEmpty() {
+        boolean response = false;
+        if (size == 0) {
+            response = true;
+        }
+        return response;
     }
 }
